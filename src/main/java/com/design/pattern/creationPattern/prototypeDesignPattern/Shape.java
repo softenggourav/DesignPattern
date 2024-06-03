1d6c254dc794c16ae01dd502492734ad28eb4c2d
softenggourav <softenggourav@gmail.com>
2024-06-03 23:03:03 +0530

    adding changes for prototypeDesignPattern

---
+++ src/main/java/com/design/pattern/creationPattern/prototypeDesignPattern/Shape.java
@@ -0,0 +1,28 @@
+package com.design.pattern.creationPattern.prototypeDesignPattern;
+
+import java.util.Objects;
+
+public abstract class Shape {
+    public int x;
+    public int y;
+    public String color;
+
+
+    public Shape() {
+
+    }
+
+    public Shape(Shape target) {
+        this.x = target.x;
+        this.y = target.y;
+        this.color = target.color;
+    }
+
+    public abstract Shape clone();
+
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Shape)) return false;
+        Shape shape2 = (Shape) object2;
+        return shape2.x == x && shape2.y == y && Objects.equals(shape2.color, color);
+    }
+}
